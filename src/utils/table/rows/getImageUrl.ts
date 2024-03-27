244163486eb9ab1da3cf5d055637fa6d0f773acd
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2024-03-27 14:51:08 +0200

    get base url from useConfig

--- src/utils/table/rows/getImageUrl.ts
+++ src/utils/table/rows/getImageUrl.ts
@@ -1,7 +1,10 @@
+import { useConfig } from "@dhis2/app-runtime"
+
 export const GetImageUrl = () => {
+    const { baseUrl } = useConfig()
 
     function imageUrl({ trackedEntity, attribute }: { attribute: string, trackedEntity: string }) {
-        return `https://emis.dhis2.org/startracker/api/trackedEntityInstances/${trackedEntity}/${attribute}/image?dimension=MEDIUM`
+        return `${baseUrl}/api/trackedEntityInstances/${trackedEntity}/${attribute}/image?dimension=MEDIUM`
     }
 
     return {
