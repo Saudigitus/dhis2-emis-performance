1b6a4571aed3676009576d3c010b2a8cbf2b600a
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2024-04-03 11:30:27 +0200

    Remove class and grade when removing school

--- src/components/layout/header/HeaderItem.tsx
+++ src/components/layout/header/HeaderItem.tsx
@@ -1,24 +1,33 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import style from "./MainHeader.module.css"
-import { DropdownButton, FlyoutMenu } from "@dhis2/ui"
-import { type HeadBarTypes } from '../../../types/headBar/HeadBarTypes'
+import {DropdownButton, FlyoutMenu} from "@dhis2/ui"
+import {type HeadBarTypes} from '../../../types/headBar/HeadBarTypes'
 import info from "../../../assets/images/headbar/info.svg"
-import { SimpleSearch } from '../../search'
+import {SimpleSearch} from '../../search'
 import classNames from 'classnames'
-import { componentMapping } from '../../../utils'
-import { useDataElementsParamMapping, useParams } from '../../../hooks'
-import { useRecoilState } from 'recoil'
-import { OuQueryString } from '../../../schema/headerSearchInputSchema'
+import {componentMapping} from '../../../utils'
+import {useDataElementsParamMapping, useParams} from '../../../hooks'
+import {useRecoilState} from 'recoil'
+import {OuQueryString} from '../../../schema/headerSearchInputSchema'
 import HeaderResetItemValue from './HeaderResetItemValue'
-import { getSelectedKey } from '../../../utils/commons/dataStore/getSelectedKey'
+import {getSelectedKey} from '../../../utils/commons/dataStore/getSelectedKey'
 
 export default function HeaderItem(props: HeadBarTypes): React.ReactElement {
-    const { label, value, placeholder, component, dataElementId, id, selected, disabled } = props;
-    const { remove } = useParams()
+    const {
+        label,
+        value,
+        placeholder,
+        component,
+        dataElementId,
+        id,
+        selected,
+        disabled
+    } = props;
+    const {remove} = useParams()
     const Component = (component != null) ? componentMapping[component] : null;
     const [openDropDown, setOpenDropDown] = useState<boolean>(false);
     const [, setStringQuery] = useRecoilState(OuQueryString);
-    const { getDataStoreData } = getSelectedKey()
+    const {getDataStoreData} = getSelectedKey()
 
     const onToggle = () => {
         setStringQuery(undefined)
@@ -28,13 +37,14 @@ export default function HeaderItem(props: HeadBarTypes): React.ReactElement {
     const paramsMapping = useDataElementsParamMapping()
 
     const onReset = () => {
-        if (dataElementId)
+        if (dataElementId) {
             remove(paramsMapping[dataElementId as unknown as keyof typeof paramsMapping])
-        else
-            if (id === "c540ac7c") {
-                remove("school");
-                remove("schoolName");
-            }
+        } else if (id === "c540ac7c") {
+            remove("school");
+            remove("schoolName");
+            remove("class");
+            remove("grade");
+        }
     }
 
     return (
@@ -44,16 +54,17 @@ export default function HeaderItem(props: HeadBarTypes): React.ReactElement {
             onClick={onToggle}
             className={classNames(style.HeaderItemContainer, style[id])}
             component={
-                < FlyoutMenu >
+                < FlyoutMenu>
                     <SimpleSearch id={id} placeholder={placeholder}>
-                        {(Component != null) && <Component dataElementId={dataElementId} onToggle={onToggle} />}
+                        {(Component != null) && <Component dataElementId={dataElementId} onToggle={onToggle}/>}
                     </SimpleSearch>
-                </FlyoutMenu >
+                </FlyoutMenu>
             }
         >
             <h5>{label} <span>{value}</span></h5>
-            {(selected && dataElementId !== getDataStoreData?.registration?.academicYear) ? <HeaderResetItemValue onReset={onReset} /> : null}
-            <img src={info} />
-        </DropdownButton >
+            {(selected && dataElementId !== getDataStoreData?.registration?.academicYear) ?
+                <HeaderResetItemValue onReset={onReset}/> : null}
+            <img src={info}/>
+        </DropdownButton>
     )
 }
