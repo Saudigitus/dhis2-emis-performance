21bbfd56efd2bd78f68e39ad3234cc6e69490a34
EdsonNhancale <edsonnhancale97@gmail.com>
2024-08-28 11:34:26 +0200

    feat: implemented programRule for the subjects according with grade

--- src/components/table/components/head/HeaderFilters.tsx
+++ src/components/table/components/head/HeaderFilters.tsx
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ConfigTableColumns from "../configTableColumns/ConfigTableColumns";
 import EnrollmentFilters from "../filters/enrollment/EnrollmentFilters";
 import SwitchButtonView from "../../../buttons/MultipleButtons/SwitchButtonView";
 import styles from './HeaderFilters.module.css'
-import { useHeader } from "../../../../hooks";
+import { useHeader, useParams } from "../../../../hooks";
 import { useRecoilState } from "recoil";
 import { TableColumnState } from "../../../../schema/columnSchema";
+import { getSelectedKey } from "../../../../utils";
+import { CustomDhis2RulesEngine } from "../../../../hooks/programRules/rules-engine/RulesEngine";
 
 function HeaderFilters() {
-  const { columns } = useHeader();
+  const { columns = [] } = useHeader();
   const [updatedCols, setTableColumns] = useRecoilState(TableColumnState)
-  
   const setTableHeaders = (tableHeaders: any) => setTableColumns(tableHeaders)
-  
+
   return (
     <div className={styles.filterContainer}>
       <EnrollmentFilters />
